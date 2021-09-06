da645b2b4b86006b4476e0f24a4ff79f9daf6976
Shawn Doyle <shawn@opslock.com>
2021-09-06 18:05:40 -0400

    feat(browse): reconfigure browse tables state

--- archetypes/Browse/FilterSelects/index.tsx
+++ archetypes/Browse/FilterSelects/index.tsx
@@ -1,109 +1,54 @@
-import { Input, SearchBar, Select, SelectOption } from '@components/General/Input';
-import { FilterState, FilterAction, SORT_MAP } from '@context/FilterContext';
-import { SIDE_MAP } from '@libs/constants';
+import { DropDown } from '@components/General';
+import { SearchInput } from '@components/General/SearchInput';
 import React from 'react';
-import styled from 'styled-components';
+import { BrowseAction, BrowseState, LeverageFilterEnum, SideFilterEnum, SortByEnum } from '../state';
 
-export default (({ filterState, filterDispatch }) => {
+interface FilterSelectsProps {
+    state: BrowseState;
+    dispatch: React.Dispatch<BrowseAction>;
+}
+
+const FilterSelects: React.FC<FilterSelectsProps> = ({ state, dispatch }) => {
     return (
-        <Filters>
-            <StyledSearchBar
-                onChange={(e: any) => filterDispatch({ type: 'setSearch', value: e.currentTarget.value })}
-            />
-            <DropdownSelect
-                options={filterState?.leverageOptions}
-                filterDispatch={filterDispatch}
-                dispatchAction={'setLeverage'}
-                title={'Leverage'}
-                selectedOption={filterState?.leverage}
-            />
-            <DropdownSelect
-                options={filterState?.sideOptions}
-                filterDispatch={filterDispatch}
-                dispatchAction={'setSide'}
-                title={'Side'}
-                selectedOption={filterState?.side === 'All' ? 'All' : SIDE_MAP[filterState?.side]}
-            />
-            <DropdownSelect
-                options={filterState?.sortOptions}
-                filterDispatch={filterDispatch}
-                dispatchAction={'setSortBy'}
-                keyMap={SORT_MAP}
-                title={'Sort'}
-                selectedOption={filterState?.sortBy}
-            />
-        </Filters>
-    );
-}) as React.FC<{
-    filterState: FilterState;
-    filterDispatch: React.Dispatch<FilterAction>;
-}>;
-
-const Filters = styled.div`
-    display: flex;
-    margin-bottom: 0.8rem;
-`;
-
-const StyledSearchBar = styled(SearchBar)`
-    height: 44px;
-    margin-top: auto;
-    & ${Input} {
-        margin-top: auto;
-        height: 44px;
-    }
-`;
-
-const DropdownSelect: React.FC<{
-    options: (string | number)[];
-    keyMap?: Record<string | number, string>;
-    filterDispatch: React.Dispatch<FilterAction>;
-    dispatchAction: 'setSide' | 'setLeverage' | 'setSortBy';
-    title: string;
-    selectedOption: string | number;
-}> = ({ options, filterDispatch, dispatchAction, selectedOption, title, keyMap }) => {
-    return (
-        <DropdownContainer>
-            <Label>{title}</Label>
-            <Select
-                preview={keyMap ? keyMap[selectedOption] : selectedOption}
-                onChange={(e: any) =>
-                    filterDispatch({ type: dispatchAction, value: e.currentTarget.value } as FilterAction)
-                }
-            >
-                {options.map((option) => (
-                    <SelectOption
-                        key={`${title}-dropdown-option-${option}`}
-                        value={option}
-                        selected={selectedOption === option}
-                    >
-                        {keyMap ? keyMap[option] : option}
-                    </SelectOption>
-                ))}
-            </Select>
-        </DropdownContainer>
+        <>
+            <section className="container hidden md:block">
+                <div className="flex w-full mb-2">
+                    <div className="mr-4 flex-grow flex items-end" style={{ maxWidth: '20rem' }}>
+                        <SearchInput
+                            placeholder="Search"
+                            value={state.search}
+                            onChange={(search) => dispatch({ type: 'setSearch', search })}
+                        />
+                    </div>
+                    <div className="mr-4">
+                        <h3 className="mb-1">Leverage</h3>
+                        <DropDown
+                            value={state.leverage}
+                            options={Object.values(LeverageFilterEnum)}
+                            onSelect={(val) => dispatch({ type: 'setLeverage', leverage: val as LeverageFilterEnum })}
+                        />
+                    </div>
+                    <div>
+                        <h3 className="mb-1">Side</h3>
+                        <DropDown
+                            value={state.side}
+                            options={Object.values(SideFilterEnum)}
+                            onSelect={(val) => dispatch({ type: 'setSide', side: val as SideFilterEnum })}
+                        />
+                    </div>
+                    <div className="flex-grow" />
+                    <div>
+                        <h3 className="mb-1">Sort</h3>
+                        <DropDown
+                            value={state.sortBy}
+                            options={Object.values(SortByEnum)}
+                            onSelect={(val) => dispatch({ type: 'setSortBy', sortBy: val as SortByEnum })}
+                        />
+                    </div>
+                </div>
+            </section>
+        </>
     );
 };
 
-const Label = styled.p`
-    // font-family: Inter;
-    font-style: normal;
-    font-weight: 500;
-    font-size: 16px;
-    color: #111928;
-`;
-
-const DropdownContainer = styled.div`
-    margin: 0 1rem;
-
-    &:last-child {
-        margin-left: auto;
-        margin-right: 0;
-    }
-
-    ${Select} {
-        width: 12.625rem;
-        height: 44px;
-        line-height: 44px;
-        padding-left: 1rem;
-    }
-`;
+export default FilterSelects;
--- archetypes/Browse/index.tsx
+++ archetypes/Browse/index.tsx
@@ -1,16 +1,36 @@
-import React, { useContext } from 'react';
-import { FilterContext, noDispatch, defaultState } from '@context/FilterContext';
+import React, { useReducer } from 'react';
 import styled from 'styled-components';
 import FilterSelects from './FilterSelects';
 import PoolsTable from './PoolsTable';
+import { Container } from '@components/General';
+import dummyPoolData from './testData';
+import InvestNav from '@components/Nav/InvestNav';
 import {
-    // Button,
-    Container,
-} from '@components/General';
-import { TEST_POOL_DATA } from './PoolsTable/testData';
+    browseReducer,
+    BrowseState,
+    BrowseTableRowData,
+    LeverageFilterEnum,
+    SideFilterEnum,
+    SortByEnum,
+} from './state';
+import { useWeb3 } from '@context/Web3Context/Web3Context';
+import { FilterFilled, SearchOutlined } from '@ant-design/icons';
 
 export const Browse: React.FC = () => {
-    const { filterState, filterDispatch } = useContext(FilterContext);
+    const { account } = useWeb3();
+    const [state, dispatch] = useReducer(browseReducer, {
+        search: '',
+        leverage: LeverageFilterEnum.All,
+        side: SideFilterEnum.All,
+        sortBy: account ? SortByEnum.MyHoldings : SortByEnum.Name,
+    } as BrowseState);
+
+    const pools = getPoolData();
+
+    function getPoolData() {
+        // Update this to fetch real data
+        return dummyPoolData;
+    }
 
     const handleBuyToken = (address: string) => {
         // Trigger buy token UX
@@ -20,25 +40,87 @@ export const Browse: React.FC = () => {
         // Trigger sell token UX
     };
 
+    const leverageFilter = (pool: BrowseTableRowData): boolean => {
+        switch (state.leverage) {
+            case LeverageFilterEnum.All:
+                return true;
+            case LeverageFilterEnum.One:
+                return pool.leverage === 1;
+            case LeverageFilterEnum.Two:
+                return pool.leverage === 2;
+            case LeverageFilterEnum.Three:
+                return pool.leverage === 3;
+            case LeverageFilterEnum.Four:
+                return pool.leverage === 4;
+            case LeverageFilterEnum.Five:
+                return pool.leverage === 5;
+            default:
+                return false;
+        }
+    };
+
+    const sideFilter = (pool: BrowseTableRowData): boolean => {
+        switch (state.side) {
+            case SideFilterEnum.All:
+                return true;
+            case SideFilterEnum.Long:
+                return pool.side === 'long';
+            case SideFilterEnum.Short:
+                return pool.side === 'short';
+            default:
+                return false;
+        }
+    };
+
+    const searchFilter = (pool: BrowseTableRowData): boolean => {
+        const searchString = state.search.toLowerCase();
+        return Boolean(pool.tokenName.toLowerCase().match(searchString));
+    };
+
+    const sorter = (poolA: BrowseTableRowData, poolB: BrowseTableRowData): number => {
+        switch (state.sortBy) {
+            case SortByEnum.Name:
+                return poolA.tokenName.localeCompare(poolB.tokenName);
+            case SortByEnum.Price:
+                return poolB.lastPrice - poolA.lastPrice;
+            case SortByEnum.Change24Hours:
+                return poolB.change24Hours - poolA.change24Hours;
+            case SortByEnum.RebalanceRate:
+                return poolB.rebalanceRate - poolA.rebalanceRate;
+            case SortByEnum.APY30Day:
+                return poolB.APY30Days - poolA.APY30Days;
+            case SortByEnum.TotalValueLocked:
+                return poolB.totalValueLocked - poolA.totalValueLocked;
+            case SortByEnum.MyHoldings:
+                return poolB.myHoldings - poolA.myHoldings;
+            default:
+                return 0;
+        }
+    };
+
+    const filteredPools = pools.filter(sideFilter).filter(leverageFilter).filter(searchFilter);
+
+    const sortedPools = filteredPools.sort(sorter);
+
     return (
-        <BrowseContainer>
-            {/* <BrowseButtons>
-                <PageButton>Exchange</PageButton>
-                <PageButton className="primary">Browse</PageButton>
-            </BrowseButtons> */}
-            <BrowseModal>
-                <Title>Pool Tokens</Title>
-                <FilterSelects
-                    filterState={filterState ?? defaultState}
-                    filterDispatch={filterDispatch ?? noDispatch}
-                />
-                <PoolsTable pools={TEST_POOL_DATA} onClickBuy={handleBuyToken} onClickSell={handleSellToken} />
-            </BrowseModal>
-        </BrowseContainer>
+        <>
+            <InvestNav left={<SearchOutlined />} right={<FilterFilled className="" />} />
+            <BrowseContainer>
+                <BrowseModal>
+                    <Title>Pool Tokens</Title>
+                    <p className="mb-1 text-gray-500">This is a list of latest transactions.</p>
+                    <FilterSelects state={state} dispatch={dispatch} />
+                    <PoolsTable rows={sortedPools} onClickBuy={handleBuyToken} onClickSell={handleSellToken} />
+                </BrowseModal>
+            </BrowseContainer>
+        </>
     );
 };
 
 const Title = styled.h1`
+    @media (max-width: 768px) {
+        display: none;
+    }
     font-style: normal;
     font-weight: bold;
     font-size: 30px;
@@ -47,35 +129,18 @@ const Title = styled.h1`
 `;
 
 const BrowseContainer = styled(Container)`
+    @media (max-width: 768px) {
+        margin-top: 0;
+    }
     margin-top: 100px;
 `;
 
-// const BrowseButtons = styled.div`
-//     margin-bottom: 1rem;
-// `;
-
 const BrowseModal = styled.div`
+    @media (max-width: 768px) {
+        padding: 0;
+    }
     background: var(--color-background);
     box-shadow: 4px 4px 50px rgba(0, 0, 0, 0.06);
     border-radius: 20px;
     padding: 48px 32px;
 `;
-
-// const PageButton = styled(Button)`
-//     width: 122px;
-//     height: 3.44rem; // 55px
-//     display: inline;
-//     background: #e5e7eb;
-//     color: #6b7280;
-//     border: none;
-
-//     &:hover {
-//         color: #fff;
-//     }
-
-//     &.primary {
-//         color: #fff;
-//         border: 1px solid var(--color-primary);
-//         margin-left: 0.5rem;
-//     }
-// `;
