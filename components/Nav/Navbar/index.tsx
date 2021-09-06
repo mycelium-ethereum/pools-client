8078d3c942cecd15a5adfcc30e1a0a2676a78e09
dospore <tetthera@gmail.com>
2021-09-06 11:31:10 +1000

    Remove switcher and set default to light

--- components/Nav/Navbar/index.tsx
+++ components/Nav/Navbar/index.tsx
@@ -122,7 +122,7 @@ export const NavBarContent = styled(({ className }) => {
 
             <CommitDropdown />
 
-            <ThemeSwitcher />
+            {/* <ThemeSwitcher /> */}
 
             <MobileMenu />
 
