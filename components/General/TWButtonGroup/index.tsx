284f5ccf14382cff1b1955d4b997a002ffac6df4
dospore <tetthera@gmail.com>
2021-09-08 17:07:27 +1000

    Add new tooltip

--- components/General/TWButtonGroup/index.tsx
+++ components/General/TWButtonGroup/index.tsx
@@ -1,12 +1,15 @@
-import React, { useRef } from 'react';
-import ReactTooltip from 'react-tooltip';
+import React from 'react';
+
+// @ts-ignore
+import ReactSimpleTooltip from 'react-simple-tooltip';
+
 import styled from 'styled-components';
 
-const SELECTED = 'z-10 ring-1 ring-blue-700 border-blue-700';
-const DISABLED = 'opacity-50 cursor-not-allowed';
-
+const SELECTED = 'z-10 bg-tracer-800 text-white ';
+const DISABLED = 'cursor-not-allowed';
+const BORDERS = 'first:rounded-l-md last:rounded-r-md';
 const DEFAULT_BUTTON =
-    'relative inline-flex items-center px-4 py-2 first:rounded-l-md last:rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none';
+    'relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 focus:outline-none';
 
 type Option = {
     key: number;
@@ -18,17 +21,17 @@ type Option = {
 
 export default (({ options, value, onClick }) => {
     return (
-        <span className="relative z-0 inline-flex shadow-sm rounded-md">
-            {options.map((option) =>
+        <span className="relative z-0 inline-flex shadow-sm">
+            {options.map((option, index) =>
                 option.disabled ? (
-                    <ComingSoon onClick={onClick} option={option} />
+                    <ComingSoon last={index === options.length - 1} onClick={onClick} option={option} />
                 ) : (
                     <button
                         type="button"
                         onClick={() => onClick(option.key)}
                         className={`${value === option.key ? SELECTED : ''} ${
                             option.disabled ? DISABLED : ''
-                        } ${DEFAULT_BUTTON} }`}
+                        } ${DEFAULT_BUTTON} ${BORDERS}}`}
                     >
                         {option.text}
                     </button>
@@ -45,28 +48,43 @@ export default (({ options, value, onClick }) => {
 const ComingSoon: React.FC<{
     option: Option;
     onClick: (key: number) => any;
-}> = ({ option, onClick }) => {
-    const ref = useRef(null);
+    last: boolean;
+}> = ({ option, onClick, last }) => {
     return (
         <>
-            <StyledTooltip className="shadow bg-gray-50" id={`${option.text}`} effect={'solid'}>
-                {option?.disabled?.text}
-            </StyledTooltip>
-            <button
-                ref={ref}
-                type="button"
-                data-tip
-                data-for={`${option.text}`}
-                onClick={() => onClick(option.key)}
-                className={`${DISABLED} ${DEFAULT_BUTTON} }`}
+            <StyledTooltip
+                content={`${option.disabled?.text}`}
+                arrow={6}
+                background="#f9fafb"
+                border="#f9fafb"
+                color="#000"
+                customCss={{
+                    whiteSpace: 'nowrap',
+                }}
+                fadeDuration={300}
+                fadeEasing="linear"
+                fixed={false}
+                fontSize="12px"
+                padding={8}
+                radius={6}
+                placement="top"
+                zIndex={1}
             >
-                {option.text}
-            </button>
+                <button
+                    type="button"
+                    data-tip
+                    data-for={`${option.text}`}
+                    onClick={() => onClick(option.key)}
+                    className={`${DISABLED} ${DEFAULT_BUTTON} ${last ? 'rounded-r-md' : ''} `}
+                >
+                    {option.text}
+                </button>
+            </StyledTooltip>
         </>
     );
 };
 
-const StyledTooltip = styled(ReactTooltip)`
+const StyledTooltip = styled(ReactSimpleTooltip)`
     &.show {
         opacity: 1 !important;
     }
