d10f276e4213cf466607e88e5f623d75351f53a3
Steven Yulong Yan <43001444+Steven-Yulong-Yan@users.noreply.github.com>
2021-09-11 17:31:47 +1000

    Add Usersnap (#47)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 // prevent creating full trace
 process.traceDeprecation = true;
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import '../styles/index.css';
@@ -13,7 +13,20 @@ import GlobalStyles from 'styles/GlobalStyles';
 import { Web3Store } from '@context/Web3Context/Web3Context';
 import { UsersCommitStore } from '@context/UsersCommitContext';
 
+const USERSNAP_GLOBAL_API_KEY = process.env.NEXT_PUBLIC_USERSNAP_GLOBAL_API_KEY;
+const USERSNAP_API_KEY = process.env.NEXT_PUBLIC_USERSNAP_API_KEY;
+
 const App = ({ Component, pageProps }: AppProps) => { // eslint-disable-line
+    useEffect(() => {
+        // @ts-ignore
+        window.onUsersnapCXLoad = function (api) {
+            // @ts-ignore
+            window.Usersnap = api;
+            api.init();
+            api.show(USERSNAP_API_KEY);
+        };
+    }, []);
+
     return (
         <div>
             <Head>
@@ -33,6 +46,11 @@ const App = ({ Component, pageProps }: AppProps) => { // eslint-disable-line
 
                 {/** Color for Chrome tabs (Android only) */}
                 <meta name="theme-color" content="#000240" />
+
+                <script
+                    async
+                    src={`https://widget.usersnap.com/global/load/${USERSNAP_GLOBAL_API_KEY}?onload=onUsersnapCXLoad`}
+                />
             </Head>
             <GlobalStyles />
             <ToastProvider components={{ Toast: Notification }}>
