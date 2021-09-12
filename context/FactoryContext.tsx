23f1f7a278087b2deef045b050d10b646a6e720f
dospore <tetthera@gmail.com>
2021-09-12 14:21:49 +1000

    Label

--- context/FactoryContext.tsx
+++ context/FactoryContext.tsx
@@ -9,18 +9,22 @@ import { ARBITRUM_RINKEBY } from '@libs/constants';
 // this is a temp hack to fix fetching pools > 2000 blocks from currentBlock
 const ARBITRUM_POOLS = [
     {
+        // 1x
         name: 'BTC/USDC',
         address: '0xC42778b0248d4630b7a792dDDEa2Af5094639e9B',
     },
     {
+        // 3x
         name: 'BTC/USDC',
         address: '0xFBa3aa7c015efdB16F74355Dad5Ba671aAF5741c',
     },
     {
+        // 1x
         name: 'ETH/USDC',
         address: '0x92cf8251ff07Ceee503dBF34352d799cC197746a',
     },
     {
+        // 3x
         name: 'ETH/USDC',
         address: '0xf69FCE9ad0d3Fc50adbcD12F4165B6709AEc6368',
     },
