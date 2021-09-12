53e7ef0473b5dae73dc13f4a449b7ca5fae18f86
dospore <tetthera@gmail.com>
2021-09-12 14:20:22 +1000

    Update pools

--- context/FactoryContext.tsx
+++ context/FactoryContext.tsx
@@ -6,6 +6,25 @@ import { PoolFactory } from '@tracer-protocol/perpetual-pools-contracts/types';
 import { PoolType } from '@libs/types/General';
 import { ARBITRUM_RINKEBY } from '@libs/constants';
 
+// this is a temp hack to fix fetching pools > 2000 blocks from currentBlock
+const ARBITRUM_POOLS = [
+    {
+        name: 'BTC/USDC',
+        address: '0xC42778b0248d4630b7a792dDDEa2Af5094639e9B',
+    },
+    {
+        name: 'BTC/USDC',
+        address: '0xFBa3aa7c015efdB16F74355Dad5Ba671aAF5741c',
+    },
+    {
+        name: 'ETH/USDC',
+        address: '0x92cf8251ff07Ceee503dBF34352d799cC197746a',
+    },
+    {
+        name: 'ETH/USDC',
+        address: '0xf69FCE9ad0d3Fc50adbcD12F4165B6709AEc6368',
+    },
+]
 interface ContextProps {
     pools: PoolType[];
 }
@@ -40,12 +59,7 @@ export const FactoryStore: React.FC<Children> = ({ children }: Children) => {
             if (contract) {
                 if (network === parseInt(ARBITRUM_RINKEBY)) {
                     // hacky temp solution to rpc limit issues
-                    setPools([
-                        {
-                            name: 'BTC/USDC',
-                            address: '0x544bD86550d9774f686030969B6E72B7538c4F9e',
-                        },
-                    ]);
+                    setPools(ARBITRUM_POOLS);
                 } else {
                     const createdMarkets = contract.filters.DeployPool();
                     const allEvents = await contract?.queryFilter(createdMarkets);
