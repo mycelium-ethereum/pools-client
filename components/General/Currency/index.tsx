df4f467d894f1f7092915b98ec230e2a14d1d9b5
Nicholas Buckeridge <bucknich@gmail.com>
2021-09-14 21:59:44 +1000

    Added pool token modals

--- components/General/Currency/index.tsx
+++ components/General/Currency/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Logo } from '../Logo';
+import { classNames } from '@libs/utils/functions';
 
 export const Currency: React.FC<{
+    className?: string;
     ticker: string;
-}> = ({ ticker }) => (
-    <div className="flex items-center h-auto p-2 mr-2 rounded-xl bg-white text-gray-500">
+    label?: string;
+}> = ({ ticker, label, className }) => (
+    <div className={classNames(className ?? '', "flex items-center h-auto p-2 mr-2 rounded-xl bg-white text-gray-500")}>
         <Logo className="inline mr-2 m-0 w-5 h-5" ticker={ticker} />
-        {ticker}
+        {label ?? ticker}
     </div>
 );
