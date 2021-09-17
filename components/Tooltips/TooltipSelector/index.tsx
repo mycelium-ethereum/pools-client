68fddbd97fad453001b94aa8d8a057671247e3aa
Steven Yulong Yan <43001444+Steven-Yulong-Yan@users.noreply.github.com>
2021-09-17 14:33:27 +1000

    Remove react simple tooltip (#137)

--- components/Tooltips/TooltipSelector/index.tsx
+++ components/Tooltips/TooltipSelector/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyledTooltip, LockTip } from '../';
 
 export type TooltipSelectorProps = {
-    key: 'lock';
+    key: string | React.ReactNode;
 };
 
 const TooltipSelector: React.FC<{ tooltip: TooltipSelectorProps }> = ({ tooltip, children }) => {
@@ -11,7 +11,7 @@ const TooltipSelector: React.FC<{ tooltip: TooltipSelectorProps }> = ({ tooltip,
             return <LockTip>{children}</LockTip>;
 
         default:
-            return <StyledTooltip>{children}</StyledTooltip>;
+            return <StyledTooltip title={tooltip.key}>{children}</StyledTooltip>;
     }
 };
 
