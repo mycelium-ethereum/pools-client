b7a9a6684266bf1019d1faf9eca417b6c41a77be
Aidan Tetther <tetthera@gmail.com>
2021-09-28 09:26:26 +1000

    Add button hover states (#178)
    * Add button hover states
    
    * Update components/Nav/FarmNav/index.tsx

--- components/Nav/FarmNav/index.tsx
+++ components/Nav/FarmNav/index.tsx
@@ -1,11 +1,15 @@
 import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
-import styled from 'styled-components';
+import { classNames } from '@libs/utils/functions';
 
 const POOLTOKEN = 0;
 const BPT = 1;
 
-// const InvestNav
+const item =
+    'inline min-w-[130px] my-auto mx-2 py-3 px-3 text-gray-700 rounded-xl transition-all cursor-pointer text-base whitespace-nowrap hover:bg-gray-50 hover:shadow-sm';
+const selected = 'bg-white hover:bg-white hover:shadow-none';
+
+// const FarmNav
 export default (({ left, right }) => {
     const router = useRouter();
 
@@ -33,48 +37,26 @@ export default (({ left, right }) => {
     };
 
     return (
-        <StakeNav>
+        <div className="relative flex w-full h-[4.5rem] text-center bg-tracer-50">
             <div className="absolute left-0 top-0 bottom-0 flex items-center">{left}</div>
             <div className="flex flex-grow justify-center">
-                <Item onClick={(_e) => handleRoute(POOLTOKEN)} selected={router.pathname === '/stakepooltoken'}>
+                <div
+                    onClick={(_e) => handleRoute(POOLTOKEN)}
+                    className={classNames(router.pathname === '/stakepooltoken' ? selected : '', item)}
+                >
                     Stake Pool Tokens
-                </Item>
-                <Item onClick={(_e) => handleRoute(BPT)} selected={router.pathname === '/stakebpt'}>
+                </div>
+                <div
+                    onClick={(_e) => handleRoute(BPT)}
+                    className={classNames(router.pathname === '/stakebpt' ? selected : '', item)}
+                >
                     Stake BPT
-                </Item>
+                </div>
             </div>
             <div className="absolute right-0 top-0 bottom-0 flex items-center">{right}</div>
-        </StakeNav>
+        </div>
     );
 }) as React.FC<{
     left?: JSX.Element;
     right?: JSX.Element;
 }>;
-
-const StakeNav = styled.div`
-    position: relative;
-    background: #eeeef6;
-    width: 100%;
-    min-height: 60px;
-    height: 60px;
-    text-align: center;
-    display: flex;
-    // justify-content: space-between;
-`;
-
-const Item = styled.div<{ selected: boolean }>`
-    width: 130px;
-    height: 44px;
-    line-height: 44px;
-    display: inline;
-    border-radius: 10px;
-    margin: auto 0;
-    cursor: pointer;
-    background: ${(props) => (props.selected ? '#fff' : 'none')};
-    transition: 0.3s;
-
-    color: #374151;
-    &:hover {
-        opacity: 0.8;
-    }
-`;
