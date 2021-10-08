8cfab955dec6eb2eac053dfba92665f8ec86efd8
dospore <tetthera@gmail.com>
2021-10-09 06:12:44 +1000

    Update onboard modal

--- pages/stakepooltoken/index.tsx
+++ pages/stakepooltoken/index.tsx
@@ -1,13 +1,26 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import NavBar from '@components/Nav/Navbar';
 import Footer from '@components/Footer';
 import { FarmStore } from '@context/FarmContext';
 import { useRouter } from 'next/router';
 import StakePool from '@archetypes/Stake/StakePool';
 import PendingCommits from '@components/PendingCommits';
+import OnboardStakeModal from '@components/OnboardModal/Stake';
 
 export default (() => {
     const router = useRouter();
+
+    const [showOnboardModal, setShowOnboardModal] = useState(false);
+    const [onboardStep, setOnboardStep] = useState<number>(1);
+
+    useEffect(() => {
+        if (localStorage.getItem('onboard.completedStakeTutorial') !== 'true') {
+            const timeout = setTimeout(() => {
+                setShowOnboardModal(true);
+            }, 3000);
+            return () => clearTimeout(timeout);
+        }
+    }, []);
 
     useEffect(() => {
         router.prefetch('/stakepooltoken');
@@ -15,12 +28,25 @@ export default (() => {
 
     return (
         <div className={`page relative matrix:bg-matrix-bg`}>
-            <NavBar />
+            <NavBar setShowOnboardModal={setShowOnboardModal} />
             <FarmStore farmContext="poolFarms">
                 <StakePool />
             </FarmStore>
             <Footer />
             <PendingCommits />
+
+            <OnboardStakeModal
+                onboardStep={onboardStep}
+                setOnboardStep={setOnboardStep}
+                showOnboardModal={showOnboardModal}
+                setShowOnboardModal={() => {
+                    setShowOnboardModal(false);
+                    localStorage.setItem('onboard.completedStakeTutorial', 'true');
+                    setTimeout(() => {
+                        setOnboardStep(1);
+                    }, 1000);
+                }}
+            />
         </div>
     );
 }) as React.FC;
