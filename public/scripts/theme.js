0f105da6c7f656bb4f0e1be0692b869fa88df838
Aidan Tetther <tetthera@gmail.com>
2021-10-14 16:36:28 +1000

    Default dark theme (#220)
    * Default dark theme
    
    * Update
    
    * Favour matrix

--- public/scripts/theme.js
+++ public/scripts/theme.js
@@ -1,12 +1,15 @@
 (function () {
     function changeTheme(inputTheme) {
-        if (inputTheme === 'dark') {
-            document.getElementsByTagName('html')[0].classList.add('theme-dark');
-            localStorage.setItem('theme', inputTheme);
-        } else if (inputTheme === 'matrix') {
-            document.getElementsByTagName('html')[0].classList.add('theme-matrix');
-            localStorage.setItem('theme', inputTheme);
-        } // else unknown theme do nothing;
+        const html = document.getElementsByTagName('html')[0];
+        if (inputTheme === 'matrix') {
+            html.classList.add('theme-matrix');
+        } else if (inputTheme === 'light') {
+            // think this is unescessary but can keep it there to ensure its light
+            html.classList.remove('theme-dark');
+        } else {
+            // else add theme dark this is default
+            html.classList.add('theme-dark')
+        } 
     }
 
     try {
