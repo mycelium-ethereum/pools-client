fb399921441edf2d8791fc5ebe4867e54c239da9
dospore <tetthera@gmail.com>
2021-10-14 10:01:17 +1000

    Animate cor logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -50,28 +50,61 @@ export default (() => {
     );
 }) as React.FC;
 
-const CorWidget: React.FC = () => (
-    <div id="cor-widget">
-        <SecurityWidget
-            right
-            color={'#3E58C9'}
-            textColor={'#ffffff'}
-            url={'https://beta.reputation.link/protocols/tracer/?network=Arbitrum'}
-        />
-        <style>{`
-            #cor-widget a {
-                background: rgba(87, 113, 226)!important;
+const CorWidget: React.FC = () => {
+
+    useEffect(() => {
+        let timeout: any;
+        if (!localStorage.getItem('animatedCorWidget')) {
+            timeout = setTimeout(() => {
+                // hacky solution since there is no selector on the element itself
+                const widget = document.getElementById('cor-widget')?.children[0]?.children[0];
+                let event = new MouseEvent('mouseenter', {
+                    'view': window,
+                    'bubbles': true,
+                    'cancelable': true
+                });
+                widget?.dispatchEvent(event)
+                timeout = setTimeout(() => {
+                    let event = new MouseEvent('mouseleave', {
+                        'view': window,
+                        'bubbles': true,
+                        'cancelable': true
+                    });
+                    widget?.dispatchEvent(event)
+                    localStorage.setItem('animatedCorWidget', 'true');
+                }, 4000)
+            }, 4000)
+        }
+        return (() => {
+            if (timeout) {
+                clearTimeout(timeout)
             }
-            @media (max-width: 1024px) {
-                #cor-widget div {
-                    bottom: 20px!important;
+        })
+    }, [])
+
+    return (
+        <div id="cor-widget">
+            <SecurityWidget
+                right
+                color={'#3E58C9'}
+                textColor={'#ffffff'}
+                url={'https://beta.reputation.link/protocols/tracer/?network=Arbitrum'}
+            />
+            <style>{`
+                #cor-widget a {
+                    background: rgba(87, 113, 226)!important;
                 }
-            }
-            @media (min-width: 1024px) {
-                #cor-widget div {
-                    bottom: 60px!important;
+                @media (max-width: 1024px) {
+                    #cor-widget div {
+                        bottom: 20px!important;
+                    }
                 }
-            }
-        `}</style>
-    </div>
-);
+                @media (min-width: 1024px) {
+                    #cor-widget div {
+                        bottom: 60px!important;
+                    }
+                }
+            `}</style>
+        </div>
+    )
+};
