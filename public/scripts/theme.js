de502cb1472547120a32afd709c62409a2df8ab6
dospore <tetthera@gmail.com>
2021-10-14 16:20:24 +1000

    Favour matrix

--- public/scripts/theme.js
+++ public/scripts/theme.js
@@ -1,10 +1,11 @@
 (function () {
     function changeTheme(inputTheme) {
         const html = document.getElementsByTagName('html')[0];
-        if (inputTheme === 'light') {
-            html.classList.remove('theme-dark');
-        } else if (inputTheme === 'matrix') {
+        if (inputTheme === 'matrix') {
             html.classList.add('theme-matrix');
+        } else if (inputTheme === 'light') {
+            // think this is unescessary but can keep it there to ensure its light
+            html.classList.remove('theme-dark');
         } else {
             // else add theme dark this is default
             html.classList.add('theme-dark')
