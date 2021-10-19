85254e78d3307f633e76c3f837ec571c1a21d35f
Steven Yulong Yan <43001444+Steven-Yulong-Yan@users.noreply.github.com>
2021-10-19 16:08:41 +1000

    Feature/add warning banner (#226)
    * Add warning banner
    
    * Add network name
    
    * Use network name from networkConfig
    
    * Add dark theme
    
    * Update text
    
    * Update to new designs
    
    * Update
    
    * Update
    
    * Update to session storage
    
    * Update
    
    * Change back network name

---
+++ components/WarningBanner/index.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react';
+import { useTheme } from '@context/ThemeContext';
+
+import Exclamation from '@public/img/warning/exclamation-circle.svg';
+import Close from '@public/img/warning/cross.svg';
+
+const WarningBanner: React.FC = () => {
+    const [showWarning, setShowWarning] = useState(false);
+    const { isDark } = useTheme();
+
+    useEffect(() => {
+        if (sessionStorage.getItem('onboard.warningShown') !== 'true') {
+            setShowWarning(true);
+        }
+    }, []);
+
+    return (
+        <div className={`${showWarning ? 'container relative' : 'hidden'}`}>
+            <div
+                className={`${
+                    isDark ? 'bg-cool-gray-700 text-white' : 'bg-cool-gray-200 text-black'
+                } px-5 py-3 absolute top-3 left-5 right-5 sm:left-0 sm:right-0 3xl:left-20 3xl:right-20 mt-2 z-50 flex items-center rounded-lg text-sm lg:text-base`}
+            >
+                <div className="mr-2">
+                    <Exclamation />
+                </div>
+                <div>
+                    <span className="font-semibold">NOTICE:</span>&nbsp;Perpetual Pools has been{' '}
+                    <a
+                        href="https://tracer.finance/radar/sigma-prime-audit-response/"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="underline text-tracer-400"
+                    >
+                        audited
+                    </a>
+                    , however the protocol is still in beta phase. Please use at your own risk.
+                </div>
+                <div
+                    className="ml-auto cursor-pointer"
+                    onClick={() => {
+                        setShowWarning(false);
+                        sessionStorage.setItem('onboard.warningShown', 'true');
+                    }}
+                >
+                    <Close fill={`${isDark ? 'white' : 'black'}`} />
+                </div>
+            </div>
+        </div>
+    );
+};
+export default WarningBanner;
