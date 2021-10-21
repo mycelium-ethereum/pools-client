d527ca22c8143f8c1e00b361c0975218e947d042
Aidan Tetther <tetthera@gmail.com>
2021-10-21 13:14:15 +1000

    Add previewURL to addNetwork (#245)
    * Add previewURL to addNetwork
    
    * Remove _ import
    
    * Remove conditional on unsupported network
    
    * Remove unecessary default and set ARBITRUM default

--- context/PoolContext/index.tsx
+++ context/PoolContext/index.tsx
@@ -22,7 +22,7 @@ import { useTransactionContext } from '@context/TransactionContext';
 import { useCommitActions } from '@context/UsersCommitContext';
 import { calcNextValueTransfer } from '@tracer-protocol/tracer-pools-utils';
 import { ArbiscanEnum, openArbiscan } from '@libs/utils/rpcMethods';
-import { networkConfig } from '@context/Web3Context/Web3Context.Config';
+import { AvailableNetwork, networkConfig } from '@context/Web3Context/Web3Context.Config';
 
 type Options = {
     onSuccess?: (...args: any) => any;
@@ -223,7 +223,8 @@ export const PoolStore: React.FC<Children> = ({ children }: Children) => {
 
             const { committer: committerInfo, keeper } = poolsState.pools[pool];
 
-            const wssProvider = networkConfig[provider?.network?.chainId]?.publicWebsocketRPC;
+            const wssProvider =
+                networkConfig[provider?.network?.chainId.toString() as AvailableNetwork]?.publicWebsocketRPC;
             const committer = new ethers.Contract(
                 committerInfo.address,
                 PoolCommitter__factory.abi,
@@ -354,7 +355,7 @@ export const PoolStore: React.FC<Children> = ({ children }: Children) => {
                     committer.commit,
                     [commitType, ethers.utils.parseUnits(amount.toFixed(), quoteTokenDecimals)],
                     {
-                        network: network,
+                        network: (network ?? '0') as AvailableNetwork,
                         onSuccess: (receipt) => {
                             console.debug('Successfully submitted commit txn: ', receipt);
                             // get and set token balances
@@ -367,7 +368,13 @@ export const PoolStore: React.FC<Children> = ({ children }: Children) => {
                                 body: (
                                     <div
                                         className="text-sm text-tracer-400 underline cursor-pointer"
-                                        onClick={() => openArbiscan(ArbiscanEnum.token, tokenAddress)}
+                                        onClick={() =>
+                                            openArbiscan(
+                                                ArbiscanEnum.token,
+                                                tokenAddress,
+                                                provider?.network?.chainId?.toString() as AvailableNetwork,
+                                            )
+                                        }
                                     >
                                         View token on Arbiscan
                                     </div>
@@ -398,7 +405,7 @@ export const PoolStore: React.FC<Children> = ({ children }: Children) => {
         const network = await signer?.getChainId();
         if (handleTransaction) {
             handleTransaction(token.approve, [pool, ethers.utils.parseEther(Number.MAX_SAFE_INTEGER.toString())], {
-                network: network,
+                network: (network ?? '0') as AvailableNetwork,
                 onSuccess: async (receipt) => {
                     console.debug('Successfully approved token', receipt);
                     poolsDispatch({
--- context/Web3Context/Web3Context.Config.ts
+++ context/Web3Context/Web3Context.Config.ts
@@ -2,6 +2,7 @@ import { PoolFactory__factory } from '@tracer-protocol/perpetual-pools-contracts
 import { StakingRewards__factory } from '@libs/staking/typechain/factories/StakingRewards__factory';
 
 import { ethers } from 'ethers';
+import { ARBITRUM, ARBITRUM_RINKEBY } from '@libs/constants';
 
 // the vault address is the same on all networks
 const BALANCER_VAULT_ADDRESS = '0xBA12222222228d8Ba445958a75a0704d566BF2C8';
@@ -14,6 +15,8 @@ type Farm = {
     link?: string;
     linkText?: string;
 };
+
+export type AvailableNetwork = typeof ARBITRUM | typeof ARBITRUM_RINKEBY | '0' | '1337';
 export type Network = {
     name: string;
     previewUrl: string;
@@ -55,7 +58,7 @@ export type Network = {
  *  this config.
  * Do not change the other network configs unless the contract addresses have changed.
  */
-export const networkConfig: Record<string, Network> = {
+export const networkConfig: Record<AvailableNetwork, Network> = {
     '0': {
         previewUrl: '',
         name: 'Unknown',
@@ -72,7 +75,7 @@ export const networkConfig: Record<string, Network> = {
     },
     '421611': {
         name: 'Arbitrum Rinkeby',
-        previewUrl: 'https://rinkeby-explorer.arbitrum.io/#',
+        previewUrl: 'https://testnet.arbiscan.io',
         contracts: {
             poolFactory: {
                 address: '0x69D044eCf45500882FdA0A796744231b1Fb5eFD5',
@@ -103,7 +106,7 @@ export const networkConfig: Record<string, Network> = {
     },
     '42161': {
         name: 'Arbitrum',
-        previewUrl: 'https://explorer.arbitrum.io/#',
+        previewUrl: 'https://arbiscan.io/',
         contracts: {
             poolFactory: {
                 address: '0x98C58c1cEb01E198F8356763d5CbA8EB7b11e4E2',
