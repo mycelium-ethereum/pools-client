2cd21b07c3d5201233b8e3d3ffe266880cf4e68f
Chris Martin <chris@crunchycloud.io>
2021-10-25 18:08:03 +1000

    [arb bridge] add warning when bridging less than $1000 USDC

--- components/General/Input/index.tsx
+++ components/General/Input/index.tsx
@@ -5,11 +5,14 @@ import React from 'react';
 export const InputContainer: React.FC<{
     className?: string;
     error?: boolean;
-}> = ({ error, className = '', children }) => (
+    warning?: boolean;
+}> = ({ error, warning, className = '', children }) => (
     <div
         className={classNames(
             'relative py-3 px-3 border rounded bg-theme-button-bg',
-            error
+            warning
+                ? 'border-yellow-500 text-yellow-600 focus-within:ring-yellow-600'
+                : error
                 ? 'border-red-300 text-red-500 focus-within:ring-red-500'
                 : 'border-theme-border text-theme-text opacity-80 focus-within:ring-theme-primary',
             'focus-within:ring-2 focus-within:ring-opacity-50 ',
