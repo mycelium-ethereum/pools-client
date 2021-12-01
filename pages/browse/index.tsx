4d66053f2ef154a311edd2cd45984be9dd5a44a3
dospore <tetthera@gmail.com>
2021-12-01 10:34:19 +1000

    Remove mint-burn page

--- pages/browse/index.tsx
+++ pages/browse/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import NavBar from '@components/Nav/Navbar';
 import Footer from '@components/Footer';
 import { Browse } from '@archetypes/Browse';
@@ -6,11 +6,15 @@ import { PoolStore } from '@context/PoolContext';
 import { useRouter } from 'next/router';
 import PendingCommits from '@components/PendingCommits';
 import { ArbitrumBridge } from '@components/ArbitrumBridge';
-import { ArbitrumBridgeStore } from '@context/ArbitrumBridgeContext';
+import { ArbitrumBridgeStore} from '@context/ArbitrumBridgeContext';
 import UnsupportedNetworkPopup from '@components/General/UnsupportedNetworkPopup';
+import { SwapStore } from '@context/SwapContext';
+import OnboardTradeModal from '@components/OnboardModal/Trade';
 
 export default (() => {
     const router = useRouter();
+    const [showOnboardModal, setShowOnboardModal] = useState(false);
+    const [onboardStep, setOnboardStep] = useState<number>(1);
 
     useEffect(() => {
         router.prefetch('/');
@@ -21,12 +25,25 @@ export default (() => {
             <PoolStore>
                 <ArbitrumBridgeStore>
                     <NavBar />
-                    <Browse />
+                    <SwapStore>
+                        <Browse />
+                    </SwapStore>
                     <PendingCommits />
                     <ArbitrumBridge />
                     <UnsupportedNetworkPopup />
                 </ArbitrumBridgeStore>
             </PoolStore>
+            <OnboardTradeModal
+                onboardStep={onboardStep}
+                setOnboardStep={setOnboardStep}
+                showOnboardModal={showOnboardModal}
+                setShowOnboardModal={() => {
+                    setShowOnboardModal(false);
+                    setTimeout(() => {
+                        setOnboardStep(1);
+                    }, 1000);
+                }}
+            />
             <Footer />
         </div>
     );
