06a14f2ccc14438a54cb2c5f6c6e7909c500c402
StevenYulongYan <yulong.yan@uqconnect.edu.au>
2021-12-14 16:04:52 +1000

    Update Matomo scripts

--- public/scripts/matomo.js
+++ public/scripts/matomo.js
@@ -1,13 +1,12 @@
 // Matomo Setup
 const _paq = (window._paq = window._paq || []);
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-_paq.push(['setCookieDomain', '*.tracer.finance']);
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function () {
     const u = 'https://tracerfinance.matomo.cloud/';
     _paq.push(['setTrackerUrl', u + 'matomo.php']);
-    _paq.push(['setSiteId', '2']);
+    _paq.push(['setSiteId', '3']);
     const d = document,
         g = d.createElement('script'),
         s = d.getElementsByTagName('script')[0];
