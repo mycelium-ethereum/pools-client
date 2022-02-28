9572fadb4c71619b6a27eee0fcdbbedeb4065257
Tim Jones <t27.jones@connect.qut.edu.au>
2022-02-28 15:51:10 +1000

    Update Matomo script, remove redirects (#500)
    * Update Matomo script, remove redirects
    
    * Disable cookies to be GDPR compliant
    
    * Add Matomo ID to help button

--- pages/_document.js
+++ pages/_document.js
@@ -32,9 +32,7 @@ export default class MyDocument extends Document {
         return (
         <Html>
             <Head>
-                <script src="/scripts/mat-script.js" />
                 <script src="/scripts/mat.js" />
-                <script src="/scripts/mat-container.js" />
                 <script src="/scripts/theme.js" />
             </Head>
             <body>
