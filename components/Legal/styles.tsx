195e78f772c13ddf18fd14f079ce57dc6306f95c
Dospore <tetthera@gmail.com>
2022-03-03 11:15:41 -0600

    chore: Remove style tag from legal component

---
+++ components/Legal/styles.tsx
@@ -0,0 +1,70 @@
+import styled from 'styled-components';
+
+export const MenuItem = styled.div<{
+    selected: boolean;
+}>`
+    margin-left: auto;
+    margin-top: 0.25rem;
+    margin-bottom: 0.25rem;
+    font-size: 1rem; /* 16px */
+    line-height: 1.5rem; /* 24px */
+    cursor: pointer;
+    white-space: nowrap;
+    text-decoration-line: underline;
+    opacity: ${(props) => (props.selected ? 1 : 0.6)};
+`;
+
+
+export const MainContent = styled.div`
+    margin: 0 auto;
+    transition-property: all;
+    transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+    transition-duration: 150ms;
+
+    max-width: 768px;
+
+    & a {
+        text-decoration: underline;
+        cursor: pointer;
+        color: var(--secondary);
+    }
+
+    & a:hover {
+        opacity: 0.8;
+    }
+`;
+
+export const MainTitle = styled.div`
+    font-weight: bold;
+    padding-top: 4rem;
+    padding-bottom: 0.75rem;
+    font-size: 1.875rem; /* 30px */
+    line-height: 2.25rem; /* 36px */
+`;
+// React.FC = ({ children }) => <div className="text-3xl font-bold pt-16 pb-3">{children}</div>;
+
+export const Title = styled.div`
+    margin-top: 1.5rem;
+    margin-bottom: 0.5rem;
+    font-size: 1.25rem; /* 20px */
+    line-height: 1.75rem; /* 28px */
+`;
+// : React.FC = ({ children }) => <div className="mt-6 mb-2 text-xl">{children}</div>;
+
+export const Subtitle = styled.div`
+    padding-bottom: 1rem;
+`;
+// export const Subtitle: React.FC = ({ children }) => <div className="pb-4 font-bold">{children}</div>;
+
+export const Paragraph = styled.div`
+    padding: 0.5rem 0;
+`;
+// export const Paragraph: React.FC = ({ children }) => <p className="py-2">{children}</p>;
+
+export const List = styled.ul`
+    margin-bottom: 1rem;
+    list-style-type: disc;
+    padding-left: 1rem;
+`;
+
+// export const List: React.FC = ({ children }) => <ul className="mb-4 list-disc pl-4">{children}</ul>;
