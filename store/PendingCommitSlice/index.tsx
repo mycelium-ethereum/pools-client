1d15b3f91ad1247fa07682643ec4d6d9ed80a663
Dospore <tetthera@gmail.com>
2022-03-31 16:00:40 +1100

    Remove focus from store

--- store/PendingCommitSlice/index.tsx
+++ store/PendingCommitSlice/index.tsx
@@ -1,11 +1,9 @@
-import { CommitActionEnum } from '@tracer-protocol/pools-js';
 import { StateSlice } from '@store/types';
 import { StoreState } from '..';
 import { IPendingCommitSlice } from './types';
 
 export const createPendingCommitSlice: StateSlice<IPendingCommitSlice> = (set) => ({
     commits: {},
-    focus: CommitActionEnum.mint,
 
     addCommit: (commitInfo) => {
         const { txnHash, pool } = commitInfo;
@@ -26,8 +24,6 @@ export const createPendingCommitSlice: StateSlice<IPendingCommitSlice> = (set) =
 
 export const selectCommits: (state: StoreState) => IPendingCommitSlice['commits'] = (state) =>
     state.pendingCommitSlice.commits;
-export const selectFocus: (state: StoreState) => IPendingCommitSlice['focus'] = (state) =>
-    state.pendingCommitSlice.focus;
 
 export const selectUserCommitActions: (state: StoreState) => {
     addCommit: IPendingCommitSlice['addCommit'];
--- store/PendingCommitSlice/types.ts
+++ store/PendingCommitSlice/types.ts
@@ -1,11 +1,7 @@
-import { CommitActionEnum } from '@tracer-protocol/pools-js';
 import { PendingCommitInfo } from '~/types/pools';
 
 export interface IPendingCommitSlice {
     commits: Record<string, Record<string, PendingCommitInfo>>;
-    // updateCommits: boolean,
-    focus: CommitActionEnum;
-
     addCommit: (commitInfo: PendingCommitInfo) => void;
     resetCommits: (pool: string) => void;
 }
