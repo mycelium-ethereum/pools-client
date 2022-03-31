bb2874bc5b57ef5e42ee45632c217bfe9b7b1bf1
Dospore <tetthera@gmail.com>
2022-03-31 16:25:54 +1100

    Add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,15 +8,25 @@ module.exports = {
         },
     },
     ignorePatterns: ['node_modules/*', 'public/static/*', 'libs/types/contracts/*'],
+    plugins: ["import"],
     settings: {
         react: {
             version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
         },
+        "import/parsers": {
+          "@typescript-eslint/parser": [".ts", ".tsx"]
+        },
+        "import/resolver": {
+          "typescript": {
+            "alwaysTryTypes": true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+          }
+        }
     },
     extends: [
         'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
         'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
         'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+        // 'plugin:import/recommended',
     ],
     rules: {
         curly: 2, // enforce brace style for control statements
@@ -36,7 +46,44 @@ module.exports = {
         'react/jsx-closing-bracket-location': 1,
         'react/jsx-closing-tag-location': 1,
         'no-multi-spaces': 1, 
-        'react/self-closing-comp': 1
+        'react/self-closing-comp': 1,
+        "import/no-unresolved": "error"
+        // 'import/order': [
+            // 1, 
+            // {
+                // "groups": [
+                  // "external", 
+                  // "builtin", 
+                  // "internal", 
+                  // "sibling", 
+                  // "parent", 
+                  // "index"
+                // ], 
+                // "pathGroups": [
+                    // { 
+                      // "pattern": 'types/ **',
+                      // "group": "internal" 
+                    // }, 
+                    // { 
+                      // "pattern": "constants/ **", 
+                      // "group": "internal" 
+                    // }, 
+                    // { 
+                      // "pattern": 'hooks/ **',
+                      // "group": "internal" 
+                    // }, 
+                    // { 
+                      // "pattern": "store/ **",
+                      // "group": "internal" 
+                    // }, 
+                 // ], 
+                // "pathGroupsExcludedImportTypes": ["internal"], 
+                // "alphabetize": { 
+                    // "order": "asc", 
+                    // "caseInsensitive": true 
+                // }
+            // } 
+        // ]
 
         // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
         // e.g. "@typescript-eslint/explicit-function-return-type": "off",
