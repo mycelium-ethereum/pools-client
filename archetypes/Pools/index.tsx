0ca597741471f5c30fa71358af681fdf1a5ad704
dweberdev <contact@dweberdev.com>
2022-04-04 13:31:03 -0700

    Pools add learn more link

--- archetypes/Pools/index.tsx
+++ archetypes/Pools/index.tsx
@@ -120,7 +120,7 @@ export const Browse: React.FC = () => {
                     <h1 className="mt-8 mb-2 px-4 sm:px-0 font-semibold text-3xl text-theme-text">Pools</h1>
                     <div className="mb-6 px-4 sm:px-0 text-sm font-light">
                         The most liquid, unique Pools with mitigated volatility decay*. Secured by Chainlink Oracles,
-                        via Tracer’s SMA Wrapper. <a className="text-tracer-400 underline">Learn More</a>
+                        via Tracer’s SMA Wrapper. <a href="https://pools-docs.tracer.finance/" target="_blank" rel="noreferrer noopener" className="text-tracer-400 underline">Learn More</a>
                     </div>
                     <FilterBar state={state} dispatch={dispatch} />
                 </section>
