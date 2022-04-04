efce250f84f9090d962be610df4f17d862efd431
Dospore <tetthera@gmail.com>
2022-04-04 11:48:03 +1000

    Update get url function

--- archetypes/Pools/PoolDetailsModal/index.tsx
+++ archetypes/Pools/PoolDetailsModal/index.tsx
@@ -22,10 +22,7 @@ export default (({ open, onClose, poolDetails, previewUrl }) => {
 
     const formatAddress = (addr: string) => `${addr?.slice(0, 4)}...${addr?.slice(40, 42)}`;
 
-    const getContractDetailsUrl = (v: string) => {
-        const BASE_URL = `${previewUrl}/address/${v}` || 'https://arbiscan.io/address/';
-        return v ? BASE_URL : 'https://arbiscan.io';
-    };
+    const getContractDetailsUrl = (v: string) => `${previewUrl}/address/${v}`
 
     const poolDetailsData = [
         { name: 'Pool Ticker', value: name },
