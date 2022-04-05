6c0d4b87e86305837bc37d73a4d4294484cd80d4
Tim Jones <t27.jones@connect.qut.edu.au>
2022-04-05 18:03:44 +1000

    Handle rankings removing disqualified entries

--- components/TradingComp/UserTable.tsx
+++ components/TradingComp/UserTable.tsx
@@ -5,6 +5,7 @@ import UserRow from '~/components/TradingComp/UserRow';
 const UserTable: React.FC<{
     data: TradingCompParticipant[];
 }> = ({ data }: { data: TradingCompParticipant[] }) => {
+    let rank = 0;
     const placeholderArr = Array.from({ length: 7 });
     const tableHeadStyles =
         'text-left text-cool-gray-900 font-semibold bg-cool-gray-100 p-4 border-b border-cool-gray-200 dark:bg-cool-gray-700 dark:text-white';
@@ -26,13 +27,14 @@ const UserTable: React.FC<{
                             .sort((a, b) => parseInt(b.accountValue) - parseInt(a.accountValue))
                             .map((item: any, index: number) => {
                                 if (!item.disqualified) {
+                                    rank += 1;
                                     return (
                                         <UserRow
                                             {...item}
                                             key={index}
-                                            rank={index + 1}
+                                            rank={index}
                                             shaded={index % 2 !== 0}
-                                            isFirst={index === 0}
+                                            isFirst={rank === 1}
                                         />
                                     );
                                 } else {
@@ -43,19 +45,22 @@ const UserTable: React.FC<{
                     </>
                 ) : (
                     <>
-                        {placeholderArr.map((item, index) => (
-                            <UserRow
-                                {...item}
-                                username={''}
-                                accountValue={''}
-                                entryDate={0}
-                                key={index}
-                                rank={index + 1}
-                                shaded={index % 2 !== 0}
-                                isFirst={index === 0}
-                                placeholder
-                            />
-                        ))}
+                        {placeholderArr.map((item, index) => {
+                            rank += 1;
+                            return (
+                                <UserRow
+                                    {...item}
+                                    username={''}
+                                    accountValue={''}
+                                    entryDate={0}
+                                    key={index}
+                                    rank={rank}
+                                    shaded={index % 2 !== 0}
+                                    isFirst={rank === 1}
+                                    placeholder
+                                />
+                            );
+                        })}
                     </>
                 )}
             </tbody>
