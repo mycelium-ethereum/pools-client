df124b5c996084d134ad8984cd1d72209d896a91
Dospore <tetthera@gmail.com>
2022-04-07 17:51:13 -0600

    Change token valuation (#584)

--- archetypes/Portfolio/Overview/index.tsx
+++ archetypes/Portfolio/Overview/index.tsx
@@ -1,5 +1,5 @@
 import React, { useReducer, useMemo } from 'react';
-import { CommitActionEnum, SideEnum } from '@tracer-protocol/pools-js';
+import { calcNotionalValue, CommitActionEnum, SideEnum } from '@tracer-protocol/pools-js';
 
 import useBrowsePools from '~/hooks/useBrowsePools';
 import useEscrowHoldings from '~/hooks/useEscrowHoldings';
@@ -55,9 +55,9 @@ export default (({ onClickCommitAction }) => {
             total += row.holdings.times(row.price).toNumber();
         });
         escrowRows.forEach((pool) => {
-            const valueInEscrow = pool.claimableLongTokens.notionalValue
-                .plus(pool.claimableShortTokens.notionalValue)
-                .plus(pool.claimableSettlementTokens.notionalValue);
+            const valueInEscrow = calcNotionalValue(pool.claimableLongTokens.currentTokenPrice, pool.claimableLongTokens.balance)
+                .plus(calcNotionalValue(pool.claimableShortTokens.currentTokenPrice, pool.claimableShortTokens.balance))
+                .plus(calcNotionalValue(pool.claimableSettlementTokens.currentTokenPrice, pool.claimableSettlementTokens.balance));
             total += valueInEscrow.toNumber();
         });
 
