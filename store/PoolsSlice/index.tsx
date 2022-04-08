ebae276846da6dfe3c7656bf5dc581423d958a43
Dospore <tetthera@gmail.com>
2022-04-07 23:51:22 -0600

    V2/update mainnet list (#589)
    * Add mainnet pool list
    
    * Add user defined type checks

--- store/PoolsSlice/index.tsx
+++ store/PoolsSlice/index.tsx
@@ -29,7 +29,6 @@ export const createPoolsSlice: StateSlice<IPoolsSlice> = (set, get) => ({
 
 export const selectImportedPools: (state: StoreState) => StaticPoolInfo[] = (state) =>
     state.web3Slice.network ? state.poolsSlice.poolLists[state.web3Slice.network]?.Imported?.pools ?? [] : [];
-
 export const selectFetchPools: (state: StoreState) => IPoolsSlice['fetchPoolLists'] = (state) =>
     state.poolsSlice.fetchPoolLists;
 export const selectImportPool: (state: StoreState) => IPoolsSlice['importPool'] = (state) =>
