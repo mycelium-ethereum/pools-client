15d4a6ab8bfa7c78166751d2a5e2491f7bcb262a
Dospore <tetthera@gmail.com>
2022-04-11 19:25:38 -0600

    Update matamo script (#596)
    * Update matamo script
    
    * Remove tag manager

--- public/scripts/matomo.js
+++ public/scripts/matomo.js
@@ -1,31 +1,23 @@
 // Matomo Setup
-const _paq = (window._paq = window._paq || []);
+var _paq = window._paq = window._paq || [];
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-_paq.push([
-    'setDomains',
-    ['*.tracer.finance', '*.docs.tracer.finance', '*.pools.tracer.finance', '*.vote.tracer.finance/'],
-]);
-_paq.push(['enableCrossDomainLinking']);
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
-(function () {
-    const u = 'https://tracerfinance.matomo.cloud/';
-    _paq.push(['setTrackerUrl', u + 'matomo.php']);
-    _paq.push(['setSiteId', '4']);
-    const d = document,
-        g = d.createElement('script'),
-        s = d.getElementsByTagName('script')[0];
-    g.async = true;
-    g.src = '//cdn.matomo.cloud/tracerfinance.matomo.cloud/matomo.js';
-    s.parentNode.insertBefore(g, s);
+(function() {
+var u="https://tracerfinance.matomo.cloud/";
+_paq.push(['setTrackerUrl', u+'matomo.php']);
+_paq.push(['setSiteId', '5']);
+var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+g.async=true; g.src='//cdn.matomo.cloud/tracerfinance.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
 })();
 
 // Matomo tag manager
-const _mtm = (window._mtm = window._mtm || []);
-_mtm.push({ 'mtm.startTime': new Date().getTime(), event: 'mtm.Start' });
-const d = document,
-    g = d.createElement('script'),
-    s = d.getElementsByTagName('script')[0];
-g.async = true;
-g.src = 'https://cdn.matomo.cloud/tracerfinance.matomo.cloud/container_9pOj4o6E.js';
-s.parentNode.insertBefore(g, s);
+// const _mtm = (window._mtm = window._mtm || []);
+// _mtm.push({ 'mtm.startTime': new Date().getTime(), event: 'mtm.Start' });
+// const d = document,
+    // g = d.createElement('script'),
+    // s = d.getElementsByTagName('script')[0];
+// g.async = true;
+// g.src = 'https://cdn.matomo.cloud/tracerfinance.matomo.cloud/container_9pOj4o6E.js';
+// s.parentNode.insertBefore(g, s);
+
