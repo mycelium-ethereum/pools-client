41b0801327bf8afa86381402e8674b849e86dcbf
dweberdev <97488700+dweberdev@users.noreply.github.com>
2022-04-12 15:09:37 -0700

    Keep modal from rendering if not in use (#591)

--- archetypes/Pools/index.tsx
+++ archetypes/Pools/index.tsx
@@ -159,12 +159,16 @@ export const Browse: React.FC = () => {
                     );
                 })}
             </div>
-            <MintBurnModal open={state.mintBurnModalOpen} onClose={handleMintBurnModalClose} />
-            <AddAltPoolModal
-                open={state.addAltPoolModalOpen}
-                onClose={handleAltModalClose}
-                sortedFilteredTokens={filteredTokens}
-            />
+            {state.mintBurnModalOpen && (
+                <MintBurnModal open={state.mintBurnModalOpen} onClose={handleMintBurnModalClose} />
+            )}
+            {state.addAltPoolModalOpen && (
+                <AddAltPoolModal
+                    open={state.addAltPoolModalOpen}
+                    onClose={handleAltModalClose}
+                    sortedFilteredTokens={filteredTokens}
+                />
+            )}
         </>
     );
 };
