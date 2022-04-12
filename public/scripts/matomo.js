3d44069f25f37240b5b3c6d47ccb009a80f5e894
Dospore <tetthera@gmail.com>
2022-04-12 10:22:12 +1000

    Remove tag manager

--- public/scripts/matomo.js
+++ public/scripts/matomo.js
@@ -12,12 +12,12 @@ g.async=true; g.src='//cdn.matomo.cloud/tracerfinance.matomo.cloud/matomo.js'; s
 })();
 
 // Matomo tag manager
-const _mtm = (window._mtm = window._mtm || []);
-_mtm.push({ 'mtm.startTime': new Date().getTime(), event: 'mtm.Start' });
-const d = document,
-    g = d.createElement('script'),
-    s = d.getElementsByTagName('script')[0];
-g.async = true;
-g.src = 'https://cdn.matomo.cloud/tracerfinance.matomo.cloud/container_9pOj4o6E.js';
-s.parentNode.insertBefore(g, s);
+// const _mtm = (window._mtm = window._mtm || []);
+// _mtm.push({ 'mtm.startTime': new Date().getTime(), event: 'mtm.Start' });
+// const d = document,
+    // g = d.createElement('script'),
+    // s = d.getElementsByTagName('script')[0];
+// g.async = true;
+// g.src = 'https://cdn.matomo.cloud/tracerfinance.matomo.cloud/container_9pOj4o6E.js';
+// s.parentNode.insertBefore(g, s);
 
