f1184d46bb034d3475c523c236e7eaa20ab50aef
Dospore <tetthera@gmail.com>
2022-05-10 09:23:39 +1000

    Allow ipfs to properly import

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const path = require('path');
 
 module.exports = {
+    assetPrefix: './', // fixes ipfs import
     cssModules: true,
     // // optional
     modifyVars: { '@primary-color': '#0000bd' },
