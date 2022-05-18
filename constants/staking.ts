6355ccb47a3d05a305c4ee0d12befb97b1c5fed6
Chris Martin <chris@crunchycloud.io>
2022-05-18 20:30:45 +1000

    add new pool token farm addresses

--- constants/staking.ts
+++ constants/staking.ts
@@ -5,17 +5,17 @@ import { StakingRewards__factory } from '~/types/staking/typechain/factories/Sta
 export const TokenToFarmAddressMap: (tokenAddress: string | null) => string = (tokenAddress) => {
     switch (tokenAddress) {
         // 3 LONG BTC/USD
-        case '0xEb05e160D3C1990719aa25d74294783fE4e3D3Ef':
-            return '0xE211c6a34a6b04Df2D5fBCf3E66Fd57b9eD76e0d';
+        case '0x2Dc6B0D6580f3E2d6107D41A6ada0d8c6c605F88':
+            return '0xcD8c0662cf72512857e98646b5C8363782c137A7';
         // 3 SHORT BTC/USD
-        case '0x02f9742f7CA51891d440084208c8e969D55b94A2':
-            return '0x50041Fe576cEC7502eA97cE33627856299011Eb1';
+        case '0x00F70af6D1148E3127DB138ce633895e5eF6Bdb2':
+            return '0x046B21659C445f43f2c621c874F79868dC6FA159';
         // 3 LONG ETH/USD
-        case '0x989132f596Ff5F79fe3e52bbDdadACde6438bF06':
-            return '0xbF3E2DbBF663b09EDCe8b774334fc408559846d0';
+        case '0x44822C092C5ece611830DC0e1B86E80645749ae8':
+            return '0xC21159bF0252A37b0c281DF2D9B723120cAa86c7';
         // 3 SHORT ETH/USD
-        case '0xe0258E0b32cD3840ef29789bcDA9C2BF996e2A40':
-            return '0xf7824Fce8C155e74E87e9575c7084D0380B55BaF';
+        case '0x466598c279C2e2B7c7f2cd591Ac539720A205582':
+            return '0x224949832f3dbf9a365D9bA3ec504727a103E96E';
         default:
             return '';
     }
@@ -40,23 +40,23 @@ export const farmConfig: Record<KnownNetwork, FarmConfig> = {
     [NETWORKS.ARBITRUM]: {
         poolFarms: [
             {
-                address: '0xE211c6a34a6b04Df2D5fBCf3E66Fd57b9eD76e0d', // 3-BTC/USD-long-farm
-                pool: '0xcf79A7Cff04C0c062E2AD8de406321737ec86Ed9',
+                address: '0xcD8c0662cf72512857e98646b5C8363782c137A7', // 3-BTC/USD-long-farm
+                pool: '0x6D3Fb4AA7ddCa8CBc88F7BA94B36ba83fF6bA234',
                 abi: StakingRewards__factory.abi,
             },
             {
-                address: '0x50041Fe576cEC7502eA97cE33627856299011Eb1', // 3-BTC/USDC-short-farm
-                pool: '0xcf79A7Cff04C0c062E2AD8de406321737ec86Ed9',
+                address: '0x046B21659C445f43f2c621c874F79868dC6FA159', // 3-BTC/USDC-short-farm
+                pool: '0x6D3Fb4AA7ddCa8CBc88F7BA94B36ba83fF6bA234',
                 abi: StakingRewards__factory.abi,
             },
             {
-                address: '0xbF3E2DbBF663b09EDCe8b774334fc408559846d0', // 3-ETH/USDC-long-farm
-                pool: '0x299bEc969417567cE31127aD7d670f0d9760684D',
+                address: '0xC21159bF0252A37b0c281DF2D9B723120cAa86c7', // 3-ETH/USDC-long-farm
+                pool: '0x3C16b9efE5E4Fc0ec3963F17c64a3dcBF7269207',
                 abi: StakingRewards__factory.abi,
             },
             {
-                address: '0xf7824Fce8C155e74E87e9575c7084D0380B55BaF', // 3-ETH/USDC-short-farm
-                pool: '0x299bEc969417567cE31127aD7d670f0d9760684D',
+                address: '0x224949832f3dbf9a365D9bA3ec504727a103E96E', // 3-ETH/USDC-short-farm
+                pool: '0x3C16b9efE5E4Fc0ec3963F17c64a3dcBF7269207',
                 abi: StakingRewards__factory.abi,
             },
         ],
