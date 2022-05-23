76b13301f6861742e3344ec7c9fa6e2f15e0b546
Dospore <tetthera@gmail.com>
2022-05-23 10:09:54 +1000

    Add typescript to test

--- hooks/usePortfolioOverview/usePortfolioOverview.test.ts
+++ hooks/usePortfolioOverview/usePortfolioOverview.test.ts
@@ -10,11 +10,11 @@ jest.mock('~/store/PendingCommitSlice');
 // const mockedPendingCommitSlice = jest.genMockFromModule('~/store/PendingCommitSlice');
 
 jest.mock('~/store/Web3Slice');
-jest.mock('../usePools');
+jest.mock('../(usePools as jest.Mock)');
 
 beforeEach(() => {
-    selectAccount.mockReturnValue(MOCK_ACCOUNT);
-    usePools.mockReturnValue({
+    (selectAccount as jest.Mock).mockReturnValue(MOCK_ACCOUNT);
+    (usePools as jest.Mock).mockReturnValue({
         pools: [
             {
                 ...DEFAULT_POOLSTATE,
@@ -22,6 +22,10 @@ beforeEach(() => {
             },
         ],
     });
+});
+
+afterEach(() => {
+    jest.clearAllMocks();
 });
 
 const MOCK_ACCOUNT = '0x9332e38f1a9BA964e166DE3eb5c637bc36cD4D27';
@@ -87,7 +91,7 @@ const LONG_TOKEN_PRICE_INCREASE = {
 
 describe('usePortfolioOverview hook', () => {
     it('usePortfolioOverview runs correctly', () => {
-        let { result } = renderHook(() => usePortfolioOverview());
+        const { result } = renderHook(() => usePortfolioOverview());
         expect(result.current.totalPortfolioValue.toNumber()).toBe(0);
         expect(result.current.unrealisedProfit.toNumber()).toBe(0);
         expect(result.current.realisedProfit.toNumber()).toBe(0);
@@ -95,7 +99,7 @@ describe('usePortfolioOverview hook', () => {
     });
 
     it('caclulates filled state', () => {
-        usePools.mockReturnValue(FILLED_INITIAL_STATE);
+        (usePools as jest.Mock).mockReturnValue(FILLED_INITIAL_STATE);
 
         const { result, rerender } = renderHook(() => usePortfolioOverview());
 
@@ -107,7 +111,7 @@ describe('usePortfolioOverview hook', () => {
         expect(result.current.portfolioDelta).toBe(0);
 
         // long token increase
-        usePools.mockReturnValue(LONG_TOKEN_PRICE_INCREASE);
+        (usePools as jest.Mock).mockReturnValue(LONG_TOKEN_PRICE_INCREASE);
         rerender();
 
         // all long tokens value increase by 10%
@@ -115,6 +119,8 @@ describe('usePortfolioOverview hook', () => {
         expect(result.current.unrealisedProfit.toNumber()).toBe(11.5);
         expect(result.current.realisedProfit.toNumber()).toBe(25);
         expect(result.current.portfolioDelta).toBe(5.111111111111112);
+
+        expect(selectAccount as jest.Mock).toHaveBeenCalledTimes(1);
     }),
         it('handles mints', () => {
             const mintAmounts = {
@@ -125,12 +131,12 @@ describe('usePortfolioOverview hook', () => {
                 longBurnShortMint: new BigNumber(25), // should ignore flip amounts for total portfolio value
                 shortBurnLongMint: new BigNumber(30), // should ignore flip amounts for total portfolio value
             };
-            selectUserPendingCommitAmounts.mockReturnValue({
+            (selectUserPendingCommitAmounts as jest.Mock).mockReturnValue({
                 ['420_69']: {
                     ['0x9332e38f1a9BA964e166DE3eb5c637bc36cD4D27']: mintAmounts,
                 },
             });
-            usePools.mockReturnValue(LONG_TOKEN_PRICE_INCREASE);
+            (usePools as jest.Mock).mockReturnValue(LONG_TOKEN_PRICE_INCREASE);
 
             const { result } = renderHook(() => usePortfolioOverview());
 
@@ -145,7 +151,7 @@ describe('usePortfolioOverview hook', () => {
             // with burns we also have to reduce from either wallet balance or aggregate balances other wise we are
             // burning from thin air. RIP when people buy on secondary market;
             // this is burning all tokens in the wallet
-            selectUserPendingCommitAmounts.mockReturnValue({
+            (selectUserPendingCommitAmounts as jest.Mock).mockReturnValue({
                 ['420_69']: {
                     ['0x9332e38f1a9BA964e166DE3eb5c637bc36cD4D27']: {
                         longBurn: new BigNumber(15),
@@ -157,7 +163,7 @@ describe('usePortfolioOverview hook', () => {
                     },
                 },
             });
-            usePools.mockReturnValue({
+            (usePools as jest.Mock).mockReturnValue({
                 pools: [
                     {
                         ...DEFAULT_POOLSTATE,
@@ -189,7 +195,7 @@ describe('usePortfolioOverview hook', () => {
             expect(result.current.portfolioDelta).toBe(5.111111111111112);
         });
     it('handles disconnected account', () => {
-        selectAccount.mockReturnValue(undefined);
+        (selectAccount as jest.Mock).mockReturnValue(undefined);
         const { result } = renderHook(() => usePortfolioOverview());
 
         // portfolio value should remain the same
