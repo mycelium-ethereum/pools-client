099178a0b93fae1b96b06867cb9294f979f6226d
Dospore <tetthera@gmail.com>
2022-05-23 17:41:44 +1000

    V2/persist route on toggle (#679)
    * Persist route on version toggle
    
    * Add comment
    
    * Remove v2 from toggle

--- components/Nav/Navbar/VersionToggle.tsx
+++ components/Nav/Navbar/VersionToggle.tsx
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useRouter } from 'next/router';
 import styled from 'styled-components';
 
-export default (({ hideOnDesktop }) => (
-    <VersionToggle hideOnDesktop={hideOnDesktop}>
-        <V1 href="https://poolsv1.tracer.finance">V1</V1>
-        <V2>
-            V2 <New>BETA</New>
-        </V2>
-    </VersionToggle>
-)) as React.FC<{
+type VersionToggleProps = {
     hideOnDesktop?: boolean;
-}>;
+};
 
-const VersionToggle = styled.span<{ hideOnDesktop?: boolean }>`
+const basePath = 'https://poolsv1.tracer.finance';
+export const VersionToggle = ({ hideOnDesktop }: VersionToggleProps): JSX.Element => {
+    const router = useRouter();
+
+    const getRoute = useCallback(() => {
+        if (router.route === '/') {
+            return `${basePath}/pools`;
+        } else {
+            // assumes that the destination base path appropriately handles redirects
+            return `${basePath}${router.route}`;
+        }
+    }, [router.route]);
+
+    return (
+        <StyledVersionToggle hideOnDesktop={hideOnDesktop}>
+            <V1 href={getRoute()}>V1</V1>
+            <V2>V2</V2>
+        </StyledVersionToggle>
+    );
+};
+
+export default VersionToggle;
+
+const StyledVersionToggle = styled.span<{ hideOnDesktop?: boolean }>`
     border: 1px solid #fff;
     border-radius: 7px;
     background-color: rgba(255, 255, 255, 0.25);
@@ -44,27 +61,11 @@ const V1 = styled.a`
 `;
 
 const V2 = styled.span`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
     background-color: #fff;
     color: #3535dc;
     border-radius: 5px;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    padding: 0 10px;
-    white-space: nowrap;
-`;
-
-const New = styled.span`
-    background-color: #3535dc;
-    color: #fff;
-    font-size: 10px;
-    font-weight: 700;
-    border-radius: 3px;
-    padding: 0 4px;
-    margin-left: 3px;
-    height: 13px;
-    display: flex;
-    align-items: center;
-    margin-bottom: -1px;
-    letter-spacing: 0;
 `;
