52ef1c669f6610ee0b1cfc596df66e29ba25c633
Chris Martin <chris@crunchycloud.io>
2022-05-31 14:11:15 +1000

    add new deprecated testnet pools

--- constants/staking.ts
+++ constants/staking.ts
@@ -6,43 +6,43 @@ export const farmConfig: Record<KnownNetwork, FarmConfig> = {
     [NETWORKS.ARBITRUM_RINKEBY]: {
         poolFarms: [
             {
-                address: '0xefD71C53282F29E8B6c7D8064a1Cd1de2E1C375d', // 4L-BTC/USD+PPUSD
-                pool: '0x4146D18b82C9a9Eb02B1ffdf1331f9563eab8cDf',
+                address: '0x4cc6094D4fc54E06b04b90Ed2F680e3D2263FDE7', // 4L-BTC/USD+PPUSD
+                pool: '0x2150D5fF4Fc13bf427183a97Dba7901Ce54471A8',
                 abi: StakingRewards__factory.abi,
             },
             {
-                address: '0x1F2A35FA2f4189670D0B4192e896c391DB3e308a', // 4S-BTC/USD+PPUSD
-                pool: '0x4146D18b82C9a9Eb02B1ffdf1331f9563eab8cDf',
+                address: '0x714d243fEaFfF5599105B59cD58bcCACb5eECc21', // 4S-BTC/USD+PPUSD
+                pool: '0x2150D5fF4Fc13bf427183a97Dba7901Ce54471A8',
                 abi: StakingRewards__factory.abi,
             },
             {
-                address: '0x966c0F7800eECf70f7425EC25f8fbD604dc5461a', // 4L-ETH/USD+PPUSD
-                pool: '0x9FBc47E8be32991Aaf1c3E59d13eA9ca20897cef',
+                address: '0x8df43395D8e01dDec63cfA584954C4201BfB103e', // 4L-ETH/USD+PPUSD
+                pool: '0x7Ca9886f9972cBB6698251F65172BE912343aAe6',
                 abi: StakingRewards__factory.abi,
             },
             {
-                address: '0x03509F6d41735D93Ba9ee4a94D8F3b8C81666742', // 4S-ETH/USD+PPUSD
-                pool: '0x9FBc47E8be32991Aaf1c3E59d13eA9ca20897cef',
+                address: '0x4E148b19d78F3EE4ffd443FCf3b8c7694F46d5C5', // 4S-ETH/USD+PPUSD
+                pool: '0x7Ca9886f9972cBB6698251F65172BE912343aAe6',
                 abi: StakingRewards__factory.abi,
             },
             {
-                address: '0xc2feb6495087baf98f1a1A3b122e688513030206', // 3L-ETH/USD+PPUSD
-                pool: '0xB5E515fcd57DA6BAc06b011057669C2e365A6959',
+                address: '0x1d3ac3f2De105F831B4eBeA8987a5f58B26aBA40', // 3L-ETH/USD+PPUSD
+                pool: '0x4022284C8aE79fe2aaeA2164aB1942e66D255bC8',
                 abi: StakingRewards__factory.abi,
             },
             {
-                address: '0xdD4bAD744E095C35a8c34157D08C8719A6a4c651', // 3S-ETH/USD+PPUSD
-                pool: '0xB5E515fcd57DA6BAc06b011057669C2e365A6959',
+                address: '0x63980B755bFE929a66605Ad527F5968A357EfC0F', // 3S-ETH/USD+PPUSD
+                pool: '0x4022284C8aE79fe2aaeA2164aB1942e66D255bC8',
                 abi: StakingRewards__factory.abi,
             },
             {
-                address: '0x759eD7CBfB35E601227E5408a103C8BD0331cEbc', // 3L-BTC/USD+PPUSD
-                pool: '0xF7844d494d16eAC8F576D014DC92D407f64a6F60',
+                address: '0x950195d6aEc6f1d5AE493ce875FEF9535C31A298', // 3L-BTC/USD+PPUSD
+                pool: '0x9726f3bDB63F5C8c98d698de2BB77e6Ee876b09B',
                 abi: StakingRewards__factory.abi,
             },
             {
-                address: '0x5573402AC9F00EE49815fe9aFfDce3c4ac1cffd6', // 3S-BTC/USD+PPUSD
-                pool: '0xF7844d494d16eAC8F576D014DC92D407f64a6F60',
+                address: '0x13394f75C2701c55011FB25e008647D283DAAB6F', // 3S-BTC/USD+PPUSD
+                pool: '0x9726f3bDB63F5C8c98d698de2BB77e6Ee876b09B',
                 abi: StakingRewards__factory.abi,
             },
         ],
--- hooks/useDeprecatedPools/index.ts
+++ hooks/useDeprecatedPools/index.ts
@@ -7,10 +7,8 @@ type DeprecatedPoolsForNetwork = Record<string, boolean>;
 
 const deprecatedPoolsLookup: Record<string, DeprecatedPoolsForNetwork> = {
     [NETWORKS.ARBITRUM_RINKEBY]: {
-        '0x4146D18b82C9a9Eb02B1ffdf1331f9563eab8cDf': true,
-        '0x9FBc47E8be32991Aaf1c3E59d13eA9ca20897cef': true,
-        '0xB5E515fcd57DA6BAc06b011057669C2e365A6959': true,
-        '0xF7844d494d16eAC8F576D014DC92D407f64a6F60': true,
+        '0x4022284C8aE79fe2aaeA2164aB1942e66D255bC8': true,
+        '0x9726f3bDB63F5C8c98d698de2BB77e6Ee876b09B': true,
     },
     [NETWORKS.ARBITRUM]: {},
 };
