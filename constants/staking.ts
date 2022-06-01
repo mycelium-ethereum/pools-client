c2886176a4fa394667db08478759e20cdc4a97ab
Chris Martin <chris@crunchycloud.io>
2022-06-01 22:15:29 +1000

    add v2.1 staking farms

--- constants/staking.ts
+++ constants/staking.ts
@@ -50,6 +50,7 @@ export const farmConfig: Record<KnownNetwork, FarmConfig> = {
     },
     [NETWORKS.ARBITRUM]: {
         poolFarms: [
+            // deprecated
             {
                 address: '0xcD8c0662cf72512857e98646b5C8363782c137A7', // 3-BTC/USD-long-farm
                 pool: '0x6D3Fb4AA7ddCa8CBc88F7BA94B36ba83fF6bA234',
@@ -68,6 +69,37 @@ export const farmConfig: Record<KnownNetwork, FarmConfig> = {
             {
                 address: '0x224949832f3dbf9a365D9bA3ec504727a103E96E', // 3-ETH/USDC-short-farm
                 pool: '0x3C16b9efE5E4Fc0ec3963F17c64a3dcBF7269207',
+                abi: StakingRewards__factory.abi,
+            },
+            // active
+            {
+                address: '0x04Ff29F8F379B2AA7d79BA66Ce76649334D83e48', // 3L-BTC/USD+USDC
+                pool: '0x3aca4F1B1791D00eBBAE01d65E9739c9C886F33C',
+                abi: StakingRewards__factory.abi,
+            },
+            {
+                address: '0x16c457fC0F5d5981574ED2BaeD81c625BD91b633', // 3S-BTC/USD+USDC
+                pool: '0x3aca4F1B1791D00eBBAE01d65E9739c9C886F33C',
+                abi: StakingRewards__factory.abi,
+            },
+            {
+                address: '0x111278bf2CC2Fd862183CF34896c60DbbEA0706F', // 3L-ETH/USD+USDC
+                pool: '0x8F4af5A3b58EA60e66690f30335Ed8586E46AcEb',
+                abi: StakingRewards__factory.abi,
+            },
+            {
+                address: '0x489dA242a948d1978673FEF8836740c11732eC0B', // 3S-ETH/USD+USDC
+                pool: '0x8F4af5A3b58EA60e66690f30335Ed8586E46AcEb',
+                abi: StakingRewards__factory.abi,
+            },
+            {
+                address: '0x38B90a6268b1a20F7A7319D533bc9d31678Cd7d8', // 3L-WTI/USD+USDC
+                pool: '0xa740b84Fd3547b9C445844c30A10Cde6dce3E08B',
+                abi: StakingRewards__factory.abi,
+            },
+            {
+                address: '0x0dC23d133cAB52be60C905C24F21925dE6e67aA2', // 3S-WTI/USD+USDC
+                pool: '0xa740b84Fd3547b9C445844c30A10Cde6dce3E08B',
                 abi: StakingRewards__factory.abi,
             },
         ],
