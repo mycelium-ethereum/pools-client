bd2c3cba0d7704a71a953f0affbc01a4afa41497
Chris Martin <chris@crunchycloud.io>
2022-06-01 17:09:52 +1000

    add mainnet deprecated pools

--- constants/deprecatedPools.ts
+++ constants/deprecatedPools.ts
@@ -6,5 +6,8 @@ export const deprecatedPools: DeprecatedPoolsForNetwork = {
         '0x4022284C8aE79fe2aaeA2164aB1942e66D255bC8': true,
         '0x9726f3bDB63F5C8c98d698de2BB77e6Ee876b09B': true,
     },
-    [NETWORKS.ARBITRUM]: {},
+    [NETWORKS.ARBITRUM]: {
+        '0x3C16b9efE5E4Fc0ec3963F17c64a3dcBF7269207': true, // 3ETH pool with out of sync SMA
+        '0x6D3Fb4AA7ddCa8CBc88F7BA94B36ba83fF6bA234': true, // 3BTC pool with out of sync SMA
+    },
 };
