464fb74ab1d9c58beee4f91553f956792a30f5d4
Dospore <tetthera@gmail.com>
2022-06-01 12:41:42 +1000

    Update DEFAULT_POOLSTATE

--- constants/pools.ts
+++ constants/pools.ts
@@ -2,11 +2,11 @@ import { BigNumber } from 'bignumber.js';
 import { KnownNetwork } from '@tracer-protocol/pools-js';
 import { NETWORKS } from '@tracer-protocol/pools-js';
 import Pool from '@tracer-protocol/pools-js/entities/pool';
-import { PoolInfo } from '~/types/pools';
+import { PoolInfo, PoolStatus } from '~/types/pools';
 
 export const DEFAULT_POOLSTATE: PoolInfo = {
     poolInstance: Pool.CreateDefault(),
-    deprecatedPool: false,
+    poolStatus: PoolStatus.Live,
     userBalances: {
         shortToken: {
             approvedAmount: new BigNumber(0),
