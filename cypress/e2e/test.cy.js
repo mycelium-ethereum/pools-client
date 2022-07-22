6faf95db29b6299fbe6478a0b099f6042d8ff72c
Tim Jones <t27.jones@connect.qut.edu.au>
2022-07-22 10:29:43 +1000

    Adjust Cypress test port depending on current environment

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,8 +1,10 @@
 const MAX_WAIT = 30000;
 
+const port = process.env.NODE_ENV === 'development' ? 3002 : 3000;
+
 describe('Testing Buy page (/)', () => {
     before(() => {
-        cy.visit('http://localhost:3002');
+        cy.visit(`http://localhost:${port}`);
     });
     it('Market dropdown should be selectable after page is fully loaded', () => {
         cy.get('.market-dropdown').should('be.visible').click(); // open market selector
@@ -14,7 +16,7 @@ describe('Testing Buy page (/)', () => {
 
 describe('Testing Trade page (/trade)', () => {
     before(() => {
-        cy.visit('http://localhost:3002/trade');
+        cy.visit(`http://localhost:${port}/trade`);
     });
     it('Trade table should be accessible after load', () => {
         cy.get('.trade-btn:first', { timeout: MAX_WAIT }); // waits to check if market dropdown exists
