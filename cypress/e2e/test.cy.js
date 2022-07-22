31a8344b4397ed88b9c9378638b3d110cad8c2d6
Tim Jones <t27.jones@connect.qut.edu.au>
2022-07-22 10:45:30 +1000

    Force starting on port 3002

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,10 +1,8 @@
 const MAX_WAIT = 30000;
 
-const port = process.env.NODE_ENV === 'development' ? 3002 : 3000;
-
 describe('Testing Buy page (/)', () => {
     before(() => {
-        cy.visit(`http://localhost:${port}`);
+        cy.visit('http://localhost:3002');
     });
     it('Market dropdown should be selectable after page is fully loaded', () => {
         cy.get('.market-dropdown').should('be.visible').click(); // open market selector
@@ -16,7 +14,7 @@ describe('Testing Buy page (/)', () => {
 
 describe('Testing Trade page (/trade)', () => {
     before(() => {
-        cy.visit(`http://localhost:${port}/trade`);
+        cy.visit('http://localhost:3002/trade');
     });
     it('Trade table should be accessible after load', () => {
         cy.get('.trade-btn:first', { timeout: MAX_WAIT }); // waits to check if market dropdown exists
