2fa2638b5593b7324710ab78841fa640886ea57c
Tim Jones <t27.jones@connect.qut.edu.au>
2022-07-27 13:39:11 +1000

    Remove screenshot, disable screenshots on `cypress run`

--- cypress.config.ts
+++ cypress.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
     e2e: {
         baseUrl: 'http://localhost:3002',
         video: false,
+        screenshotOnRunFailure: false,
         supportFile: './cypress/support/index.js',
         specPattern: './cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     },
