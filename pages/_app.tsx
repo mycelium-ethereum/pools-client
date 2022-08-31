a2fa8cbed3615dc8cc455d5a94280290402bb2e8
doylio <34788350+doylio@users.noreply.github.com>
2022-08-31 11:32:07 -0400

    Remove apostrophe

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,7 +22,7 @@ const App: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
                         <Banner>
                             <BannerTitle>ARBITRUM NITRO UPGRADE IN PROGRESS</BannerTitle>
                             <BannerContent>
-                                The Arbitrum Network is expected to have 2-4 hours of downtime as it makes it's upgrade
+                                The Arbitrum Network is expected to have 2-4 hours of downtime as it makes its upgrade
                                 to Nitro. Trades may not go through during this period.
                             </BannerContent>
                         </Banner>
